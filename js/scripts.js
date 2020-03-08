dfb7b4cd1957f8ccd775bd7b73fe499b13472352
Sree Charitha <38373549+RSreeCharitha@users.noreply.github.com>
2020-03-08 09:41:38 +0530

    Create scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function namecolor(){
+    var c = ['f35588','05dfd7','ccda46','a278b5','393e46','382039','f64b3c']
+    var  n = Math.round(Math.random()*(c.length-1))
+    console.log(c[n]);
+    document.getElementById("name").style.color='#'+c[n];
+
+}
+
+/*function nameremove(){
+    document.getElementById("name").style.color='#000';
+}*/
+
